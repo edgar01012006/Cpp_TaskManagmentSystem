3dd7188c745bc03367d93f7ee2154142fb569fd8
edgar01012006 <edgarsalardzorcyan2006@gmail.com>
2025-06-19 00:30:41 +0400

    updated Task.cpp

--- Task.cpp
+++ Task.cpp
@@ -75,3 +75,56 @@ Task& Task::operator=(Task&& rhs) {
 Task::~Task() {
     //empty;
 }
+
+//methods
+void Task::edit(const std::string& newTitle, const std::string& newDescription, const std::string& newDeadline, Task_Category newCategory, Task_Priority newPriority) 
+{
+    m_title = newTitle;
+    m_description = newDescription;
+    m_deadline = newDeadline;
+    m_category = newCategory;
+    m_priority = newPriority;
+}
+
+void Task::set_status(Task_Status status) {
+    m_status = status;
+}
+
+void Task::set_priority(Task_Priority priority) {
+    m_priority = priority;
+}
+
+std::string Task::categoryToString(Task_Category category) const {
+    switch (category) {
+        case Task_Category::NONE: return "NONE";
+        case Task_Category::WORK: return "WORK";
+        case Task_Category::STUDY: return "STUDY";
+        case Task_Category::PERSONAL: return "PERSONAL";
+    }
+}
+
+std::string Task::priorityToString(Task_Priority priority) const {
+    switch (priority) {
+        case Task_Priority::NONE: return "NONE";
+        case Task_Priority::LOW: return "LOW";
+        case Task_Priority::MID: return "MID";
+        case Task_Priority::HIGH: return "HIGH";
+        case Task_Priority::URGENT: return "URGENT";
+    }
+}
+std::string Task::statusToString(Task_Status status) const {
+    switch (status) {
+        case Task_Status::NONE: return "NONE";
+        case Task_Status::NOT_STARTED: return "NOT_STARTED";
+        case Task_Status::IN_PROGRESS: return "IN_PROGRESS";
+        case Task_Status::COMPLETED: return "COMPLETED";
+    }
+}
+
+void Task::display_info() const {
+    std::cout << "Title: " << m_title << "\nDescription: " << m_description << "\nDeadline: " << m_deadline << "\nCategory: " << categoryToString(m_category) << "\nPriority: " << priorityToString(m_priority) << "\nStatus: " << statusToString(m_status) << std::endl;
+}
+
+size_t Task::get_task_id() const {
+    return m_task_id;
+}
