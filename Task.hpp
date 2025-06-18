8d745ac4a4f4bbc2e9b1fc1c59c610d3ba250795
edgar01012006 <edgarsalardzorcyan2006@gmail.com>
2025-06-19 00:31:04 +0400

    updated Task.hpp

--- Task.hpp
+++ Task.hpp
@@ -3,15 +3,16 @@
 
 #include <iostream>
 #include <string>
-#include "User.hpp"
 
 enum class Task_Category {
+	NONE,
 	WORK,
 	STUDY,
 	PERSONAL
 };
 
 enum class Task_Priority {
+	NONE,
 	LOW,
 	MID,
 	HIGH,
@@ -19,6 +20,7 @@ enum class Task_Priority {
 };
 
 enum class Task_Status {
+	NONE,
 	NOT_STARTED,
 	IN_PROGRESS,
 	COMPLETED
@@ -27,7 +29,7 @@ enum class Task_Status {
 class Task {
 	private:
 		static size_t m_task_id; // check later
-		std::string m_uid; // user id
+		//std::string m_user_id; // user id why? what's the need for it?
 		std::string m_title; 
 		std::string m_description;
 		std::string m_deadline; 
@@ -36,7 +38,7 @@ class Task {
 		Task_Status m_status;
 	public:
 		/* constructors */
-		Task(const std::string& title, const std::string& description, const std::string& deadline, Task_Category category, TASK_Priority priority);
+		Task(const std::string& title, const std::string& description, const std::string& deadline, Task_Category category, Task_Priority priority);
 		Task(const Task& rhs);
 		Task& operator=(const Task& rhs);
 		Task(Task&& rhs);
@@ -46,9 +48,12 @@ class Task {
 		/* methods */
 		void edit(const std::string& newTitle, const std::string& newDescription, const std::string& newDeadline, Task_Category newCategory, Task_Priority newPriority);
 		void set_status(Task_Status status);
-		//void set_priority(Task_Priority prority);
-		void display_info();
-		size_t get_task_id();
+		void set_priority(Task_Priority priority);
+		std::string categoryToString(Task_Category category) const;
+		std::string priorityToString(Task_Priority priority) const;
+		std::string statusToString(Task_Status status) const;
+		void display_info() const;
+		size_t get_task_id() const;
 };
 
 #endif /* TASK_HPP */
